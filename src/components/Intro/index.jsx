a1186d3d0a0b0702410cbdf289ea47b8c1f3e24b
TranPhucTien <tranphuctien2003nd@gmail.com>
2022-06-12 11:22:23 +0700

    fix intro link in mobile

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -12,7 +12,27 @@ function Intro() {
     let _iOSDevice = !!navigator.platform.match(/iPhone|iPod|iPad/);
     let facebookLink = 'facebook.com/100009531825526'; // Open facebook in web
 
-    if (_iOSDevice) {
+    function getMobileOperatingSystem() {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        // Windows Phone must come first because its UA also contains "Android"
+        if (/windows phone/i.test(userAgent)) {
+            return 'Windows Phone';
+        }
+
+        if (/android/i.test(userAgent)) {
+            return 'Android';
+        }
+
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return 'iOS';
+        }
+
+        return 'unknown';
+    }
+
+    if (getMobileOperatingSystem() === 'iOS') {
         facebookLink = 'fb://page/?id=100009531825526'; // Open facebook app in ios
     }
 
