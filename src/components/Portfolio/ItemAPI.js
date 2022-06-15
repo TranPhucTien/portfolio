ac06cce8f7dbf6184b39cd035fed6ede1ff77a38
TranPhucTien <tranphuctien2003nd@gmail.com>
2022-06-15 21:34:26 +0700

    Replace gap with margin (flex gap not working)

--- src/components/Portfolio/ItemAPI.js
+++ src/components/Portfolio/ItemAPI.js
@@ -1,16 +1,25 @@
-import musicPlayer from '~/assets/images/music_player.png'
-import beyond from '~/assets/images/beyond.png'
-import helling from '~/assets/images/helling.png'
-import light from '~/assets/images/light.png'
+import musicPlayer from '~/assets/images/music_player.png';
+import beyond from '~/assets/images/beyond.png';
+import helling from '~/assets/images/helling.png';
+import light from '~/assets/images/light.png';
+import pokedex from '~/assets/images/pokedex.png';
 
 export const itemsAPI = [
+    {
+        image: pokedex,
+        title: 'PokeDex',
+        tags: ['JavaScript', 'HTML', 'SASS'],
+        desc: 'Tổng hợp hàng ngàn pokemon',
+        href: 'https://phuctien-pokedex.vercel.app/',
+        code: 'https://github.com/TranPhucTien/PokeDex',
+    },
     {
         image: musicPlayer,
         title: 'Music Player',
         tags: ['JavaScript', 'HTML', 'CSS'],
         desc: 'Một trình phát nhạc có đầy đủ các chức năng',
         href: 'https://tranphuctien.github.io/music_player/',
-        code: 'https://github.com/TranPhucTien/music_player'
+        code: 'https://github.com/TranPhucTien/music_player',
     },
     {
         image: beyond,
@@ -18,7 +27,7 @@ export const itemsAPI = [
         tags: ['Pug', 'Sass', 'HTML', 'CSS'],
         desc: 'Một website để mình luyện Pug và SASS',
         href: 'https://tranphuctien.github.io/Beyond./',
-        code: 'https://github.com/TranPhucTien/Beyond.'
+        code: 'https://github.com/TranPhucTien/Beyond.',
     },
     {
         image: helling,
@@ -26,7 +35,7 @@ export const itemsAPI = [
         tags: ['SASS', 'HTML', 'CSS'],
         desc: 'Mình Luyện SASS Và HTML, CSS',
         href: 'https://tranphuctien.github.io/helling/',
-        code: 'https://github.com/TranPhucTien/helling'
+        code: 'https://github.com/TranPhucTien/helling',
     },
     {
         image: light,
@@ -34,6 +43,6 @@ export const itemsAPI = [
         tags: ['JavaScript', 'HTML', 'CSS'],
         desc: 'Chỉ là một cái bóng đèn với chức năng bật tắt thôi^^',
         href: 'https://tranphuctien.github.io/light/',
-        code: 'https://github.com/TranPhucTien/light'
+        code: 'https://github.com/TranPhucTien/light',
     },
 ];
